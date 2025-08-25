f03aa2c62063860bc5fc2707f3410b7dc7480855
norafigueroa <nelizabeth.figueroa@gmail.com>
2025-08-25 09:21:32 -0600

    Actualización final

--- src/js/computadora.js
+++ src/js/computadora.js
@@ -4,13 +4,22 @@ const nombreJugadorInput = document.getElementById("nombreJugador");
 const botonGuardar = document.getElementById("botonGuardar");
 const turno = document.getElementById("turno");
 const tipoJuego = document.getElementById("tipoJuego");
-const botonConfirmar = document.getElementById("botonConfirmar");
+const botonNuevoJuego = document.getElementById("botonNuevoJuego");
 const botonReiniciar = document.getElementById("botonReiniciar");
 const botonEstadisticas = document.getElementById("botonEstadisticas")
+
+const urlParams = new URLSearchParams(window.location.search);
+const modo = urlParams.get("modo"); 
+
+  if (modo === "computadora") {
+    modalComputadora.style.display = "block";
+    // Abre automáticamente el modal cuando se redirige de multijugador 
+  }
 
 //Creación de variables
 let jugadorComp = "";   
 let juegoActivo = true; 
+let turnoJugador = true;  
 
 const combinacionesGanadoras = [
   [0, 1, 2],
@@ -23,7 +32,41 @@ const combinacionesGanadoras = [
   [2, 4, 6]
 ];
 
-function revisarGanador() { //Función para definir ganador
+function obtenerJugadores() {
+    return JSON.parse(localStorage.getItem("jugadores")) || [];
+}
+
+function guardarJugadores(lista) {
+    localStorage.setItem("jugadores", JSON.stringify(lista));
+}
+
+function actualizarEstadisticas(nombre, resultado) {
+    const jugadores = obtenerJugadores();
+    const jugadorExistente = jugadores.find(jugador => jugador.nombre === nombre);
+
+    if (jugadorExistente) {
+        if (resultado === "victoria") jugadorExistente.victorias += 1;
+        if (resultado === "derrota") jugadorExistente.derrotas += 1;
+        if (resultado === "empate") jugadorExistente.empates += 1;
+    } else {
+        const nuevoJugador = {
+          nombre: nombre,
+          victorias: 0,
+          derrotas: 0,
+          empates: 0
+        };
+
+        if (resultado === "victoria") nuevoJugador.victorias = 1;
+        if (resultado === "derrota") nuevoJugador.derrotas = 1;
+        if (resultado === "empate") nuevoJugador.empates = 1;
+
+        jugadores.push(nuevoJugador);
+      }
+      
+      guardarJugadores(jugadores);
+}
+
+function revisarGanador() {
   for (const comb of combinacionesGanadoras) {
     const [a,b,c] = comb;
 
@@ -35,6 +78,40 @@ function revisarGanador() { //Función para definir ganador
     }
   }
   return null;
+}
+
+function verificarResultado() {  
+    const ganador = revisarGanador();  
+
+    if (ganador) {
+        juegoActivo = false;  
+        turno.textContent = "";  
+        setTimeout(() => {
+            alert("¡Ganó " + (ganador === "X" ? jugadorComp : "Computadora") + "!");
+
+            if (ganador === "X") {
+                actualizarEstadisticas(jugadorComp, "victoria");
+            } else {
+                actualizarEstadisticas(jugadorComp, "derrota");
+              }
+        }, 100);
+
+        return true; 
+    }
+
+    let tableroLleno = Array.from(celdas).every(celda => celda.textContent !== "");
+    if (tableroLleno) {
+        juegoActivo = false;
+        turno.textContent = "";
+        setTimeout(() => {
+            alert("¡Empate!");
+            actualizarEstadisticas(jugadorComp, "empate");
+        }, 100);
+
+        return true;
+    }
+
+    return false;
 }
 
 function jugadaComputadora() { //Función juego aleatorio de la computadora
@@ -55,65 +132,51 @@ function jugadaComputadora() { //Función juego aleatorio de la computadora
 
 function clickJugador(event) {
   const celda = event.target;
-  if (!juegoActivo || celda.textContent !== "") return;
+  if (!juegoActivo || !turnoJugador || celda.textContent !== "") return; //No marcar en celdas ocupadas
 
   celda.textContent = "X"; // El jugador siempre juega con "X"
 
-  let ganador = revisarGanador();
-  if (ganador) {
-    juegoActivo = false;
-    setTimeout(() => {
-      alert("¡Ganó " + (ganador === "X" ? jugadorComp : "Computadora") + "!");
-    }, 100);
-    return;
+  if (verificarResultado()) return;
+
+  turnoJugador = false;                
+  turno.textContent = "Juega Computadora"
+
+  for (let index = 0; index < celdas.length; index++) {
+    celdas[index].style.pointerEvents = "none";
   }
 
-  // Si tablero no lleno, turno de computadora
-  let tableroLleno = Array.from(celdas).every(celda => celda.textContent !== "");
-  if (!tableroLleno) {
-    setTimeout(() => {
-      jugadaComputadora();
+  setTimeout(() => {
+    jugadaComputadora();
+    if (verificarResultado()) return;
 
-      ganador = revisarGanador(); //Victoria
-      if (ganador) {
-        juegoActivo = false;
-        setTimeout(() => {
-          alert("¡Ganó " + (ganador === "X" ? jugadorComp : "Computadora") + "!");
-        }, 100);
-        return;
-      }
+    turnoJugador = true; //Devuelve el turno al jugador
+    turno.textContent = jugadorComp + " juega";
 
-      tableroLleno = Array.from(celdas).every(c => c.textContent !== ""); //Empate
-      if (tableroLleno) {
-        juegoActivo = false;
-        setTimeout(() => alert("¡Empate!"), 100);
-      }
+    for (let index = 0; index < celdas.length; index++) {
+        celdas[index].style.pointerEvents = "auto";
+    }
+
     }, 300); 
-  }
 }
-
-botonConfirmar.addEventListener("click", function () { //Función para botón confirmar 
-  const opcion = tipoJuego.value;
-
-  if (opcion === "computadora") { //Abre modal para juego contra computadora
-    modalComputadora.style.display = "block";
-  } else if (opcion === "multijugador") { //Redirige a la página de multijugador
-    window.location.href = "./multijugador.html";
-  }
-});
 
 botonGuardar.addEventListener("click", function() { //Función para botón guardar
   jugadorComp = nombreJugadorInput.value.trim() || "Jugador 1";
 
+  const jugadores = JSON.parse(localStorage.getItem("jugadores")) || [];
+  const jugadorObjeto = {
+      nombre: jugadorComp,
+      victorias: 0,
+      derrotas: 0,
+      empates: 0
+  };
+
+  jugadores.push(jugadorObjeto);
+  localStorage.setItem("jugadores", JSON.stringify(jugadores));
+
   modalComputadora.style.display = "none"; // Ocultar modal
   nombreJugadorInput.value = "";
-
-  
-  for (let index = 0; index < celdas.length; index++) {
-    celdas[index].addEventListener("click", clickJugador);
-  }
-
   turno.textContent = jugadorComp + " comienza"; 
+  botonNuevoJuego.style.display = "inline-block";
 });
 
 botonReiniciar.addEventListener("click", function() {
@@ -121,5 +184,32 @@ botonReiniciar.addEventListener("click", function() {
     celdas[index].textContent = "";
   }
   juegoActivo = true;
+  turnoJugador = true; 
   turno.textContent = "";
+
+});
+
+tipoJuego.addEventListener("change", function() {
+  const opcion = tipoJuego.value;
+
+  if (opcion === "computadora") {
+    modalComputadora.style.display = "block"; // Se muestra modal al elegir opción computadora
+  } else if (opcion === "multijugador") {
+    window.location.href = "multijugador.html?modo=multijugador&abrirModal=true"; // Redirige a opción multijugador
+  }
+});
+
+for (let index = 0; index < celdas.length; index++) {
+  celdas[index].addEventListener("click", clickJugador);
+}
+
+botonNuevoJuego.addEventListener("click", function() {
+  modalComputadora.style.display = "block"; // Abrir modal
+  turno.textContent = "";
+  juegoActivo = true;
+  turnoJugador = true;
+  for (let index = 0; index < celdas.length; index++) {
+    celdas[index].textContent = "";
+  }
+  botonNuevoJuego.style.display = "none"; // Ocultar hasta que se guarde nuevo nombre
 });
--- src/js/multijugador.js
+++ src/js/multijugador.js
@@ -1,21 +1,28 @@
 const celdas = document.getElementsByClassName("celda")
 const tipoJuego = document.getElementById("tipoJuego")
-const botonConfirmar = document.getElementById("botonConfirmar")
 const modalMultijugador = document.getElementById("modalMultijugador")
 const nombreJugador1 = document.getElementById("nombreJugador1")
 const nombreJugador2 = document.getElementById("nombreJugador2")
 const botonGuardar = document.getElementById("botonGuardar")
 const turno = document.getElementById("turno");
+const botonNuevoJuego = document.getElementById("botonNuevoJuego");
 const botonReiniciar = document.getElementById("botonReiniciar")
 const botonEstadisticas = document.getElementById("botonEstadisticas")
+const mostrarEstadisticas = document.getElementById("mostrarEstadisticas")
 
+const urlParams = new URLSearchParams(window.location.search);
+const abrirModal = urlParams.get("abrirModal");
+
+if (abrirModal === "true") {
+    modalMultijugador.style.display = "block";
+    // Abre automáticamente el modal cuando se redirige de computadora
+}
 
 //Creación de variables
 let jugador = true
 let jugador1 = "Jugador 1"
 let jugador2 = "jugador 2"
-//let turnoJugador; Revisar si se debe eliminar
-let juegoActivo = true;
+let juegoActivo = false;
 
 const combinacionesGanadoras = [
   [0, 1, 2],
@@ -27,6 +34,69 @@ const combinacionesGanadoras = [
   [0, 4, 8],
   [2, 4, 6]
 ];
+
+function obtenerJugadores() {
+    return JSON.parse(localStorage.getItem("jugadores")) || [];
+}
+
+function guardarJugadores(lista) {
+    localStorage.setItem("jugadores", JSON.stringify(lista));
+}
+
+function actualizarEstadisticas(nombre, resultado) {
+    const jugadores = obtenerJugadores();
+    let jugadorExistente = jugadores.find(jugador => jugador.nombre === nombre);
+
+    if (jugadorExistente) {
+        if (resultado === "victoria") jugadorExistente.victorias += 1;
+        if (resultado === "derrota") jugadorExistente.derrotas += 1;
+        if (resultado === "empate") jugadorExistente.empates += 1;
+    } else {
+        const nuevoJugador = {nombre, victorias:0, derrotas:0, empates:0};
+        if (resultado === "victoria") nuevoJugador.victorias = 1;
+        if (resultado === "derrota") nuevoJugador.derrotas = 1;
+        if (resultado === "empate") nuevoJugador.empates = 1;
+        jugadores.push(nuevoJugador);
+    }
+
+    guardarJugadores(jugadores);
+}
+
+function verificarResultado() {  
+    const ganador = revisarGanador();  
+
+    if (ganador) {
+        juegoActivo = false;  
+        turno.textContent = "";  
+        setTimeout(() => {
+            alert("¡Ganó " + (ganador === "X" ? jugador1 : jugador2) + "!");
+            if (ganador === "X") {
+                actualizarEstadisticas(jugador1, "victoria");
+                actualizarEstadisticas(jugador2, "derrota");
+            } else {
+                actualizarEstadisticas(jugador2, "victoria");
+                actualizarEstadisticas(jugador1, "derrota");
+            }
+        }, 100);
+
+        return true;
+    }
+
+    let tableroLleno = Array.from(celdas).every(celda => celda.textContent !== "");
+    if (tableroLleno) {
+        juegoActivo = false;
+        turno.textContent = "";
+        setTimeout(() => {
+            alert("¡Empate!");
+            actualizarEstadisticas(jugador1, "empate");
+            actualizarEstadisticas(jugador2, "empate");
+        }, 100);
+
+        return true;
+    }
+
+    return false;
+}
 
 function revisarGanador() { //Función para definir ganador
 
@@ -64,84 +134,99 @@ for (let index = 0; index < celdas.length; index++) {
             turno.textContent = "Es el turno de: " + jugador1;
             } 
 
-        const ganador = revisarGanador();
-        if (ganador) {
-            juegoActivo = false; 
-            turno.textContent = ""; 
-            setTimeout(() => {
-                alert("¡Ganó " + (ganador === "X" ? jugador1 : jugador2) + "!");
-            }, 100);
-        } else {
-            let tableroLleno = true;
-                for (let index = 0; index < celdas.length; index++) {   
-                    if (celdas[index].textContent === "") {
-                        tableroLleno = false;
-                        break;
-                    }
-                }
-
-            if (tableroLleno) {
-                juegoActivo = false;
-                turno.textContent = "";
-                setTimeout(() => {
-                    alert("¡Empate!");
-                }, 100);
-            } 
-        }      
+        verificarResultado();            
     });
 }
 
-botonConfirmar.addEventListener("click", function() { //Botón para confirmar el modo de juego
+tipoJuego.addEventListener("change", function() { 
     const opcionJuego = tipoJuego.value;
-    
-    if (opcionJuego === "multijugador") { //Opción juego multijugador
-        modalMultijugador.style = "display: block";
-    }
+
+    if (opcionJuego === "multijugador") {
+        modalMultijugador.style.display = "block";
+    } 
     else if (opcionJuego === "computadora") {
-        window.location.href = "computadora.html"; //Redirige a la página de computadora
+        window.location.href = "computadora.html?modo=computadora";
     }
-});
-
-botonReiniciar.addEventListener("click", function() { //Función para botón reiniciar
-    for (let index = 0; index < celdas.length; index++) {
-        celdas[index].textContent = ""; 
-    }
-
-    jugador = true;
-    juegoActivo = true;
-    turno.textContent = jugador1 + " comienza";
-})        
+});    
 
 botonGuardar.addEventListener("click", function() { //Botón para guardar los nombres de los jugadores
 
     const jugadores = JSON.parse(localStorage.getItem("jugadores")) || [];
-    
-    const jug1 = {
-        nombre: nombreJugador1.value.trim() || "Jugador 1",
-        victorias: 0,
-        derrotas: 0,
-        empates: 0
+
+    const nombre1 = nombreJugador1.value.trim() || "Jugador 1";
+    const nombre2 = nombreJugador2.value.trim() || "Jugador 2";
+
+    if (!jugadores.some(jugador => jugador.nombre === nombre1)) {
+        jugadores.push({ 
+            nombre: nombre1, 
+            victorias: 0, 
+            derrotas: 0, 
+            empates: 0 
+        });
     }
-            
-    const jug2 = {
-        nombre: nombreJugador2.value.trim() || "Jugador 2",
-        victorias: 0,
-        derrotas: 0,
-        empates: 0
+    if (!jugadores.some(jugador => jugador.nombre === nombre2)) {
+        jugadores.push({ 
+            nombre: nombre2, 
+            victorias: 0, 
+            derrotas: 0, 
+            empates: 0 
+        });
     }
-        
-    jugadores.push(jug1);
-    jugadores.push(jug2);
 
-    localStorage.setItem("jugadores",JSON.stringify(jugadores));
+    localStorage.setItem("jugadores", JSON.stringify(jugadores));
 
-    jugador1 = jug1.nombre;
-    jugador2 = jug2.nombre;
-    turno.textContent = nombreJugador1.value + " comienza"; 
+    jugador1 = nombre1;
+    jugador2 = nombre2;
+    turno.textContent = jugador1 + " comienza";
 
-    modalMultijugador.style.display = "none"; //Cierra el modal
+    modalMultijugador.style.display = "none";
+    botonNuevoJuego.style.display = "inline-block";
 
     nombreJugador1.value = "";
     nombreJugador2.value = "";
+
+    juegoActivo = true;
 })
 
+botonReiniciar.addEventListener("click", function() {
+    for (let index = 0; index < celdas.length; index++) {
+        celdas[index].textContent = "";
+    }
+    jugador = true;
+    juegoActivo = true;
+    turno.textContent = jugador1 + " comienza";
+});
+
+    botonNuevoJuego.addEventListener("click", function() {
+
+    modalMultijugador.style.display = "block"; //Muestra modal para ingresar nuevos nombres
+
+    botonNuevoJuego.style.display = "none"; //Oculta el botón mientras se ingresan nombres
+
+    for (let index = 0; index < celdas.length; index++) {
+        celdas[index].textContent = "";
+    }
+    juegoActivo = true;
+    jugador = true;
+    turno.textContent = "";
+});
+
+botonEstadisticas.addEventListener("click", function() {
+    mostrarEstadisticas.style.display = "block";
+    mostrarEstadisticas.innerHTML = "";
+
+    const usuarios = JSON.parse(localStorage.getItem("jugadores")) || [];
+
+    for (let index = 0; index < usuarios.length; index++) {
+        const element = usuarios[index];
+
+        const informacionUsuario = document.createElement("p");
+        informacionUsuario.textContent = 
+            "Nombre: " + element.nombre +
+            " | Victorias: " + element.victorias +
+            " | Derrotas: " + element.derrotas +
+            " | Empates: " + element.empates;
+
+        mostrarEstadisticas.appendChild(informacionUsuario);
+    }
+});
